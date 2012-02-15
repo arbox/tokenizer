63beb2700089c820204f9069b557725bd8251545
Andrei Beliankou <arbox@yandex.ru>
2012-02-15 19:20:16 +0100

    Naive-naive version.

--- lib/tokenizer/tokenizer.rb
+++ lib/tokenizer/tokenizer.rb
@@ -10,31 +10,58 @@ module Tokenizer
     FS = Regexp.new('[[:blank:]]+')
 #    PRE = '[{(\\`"‚„†‡‹‘’“”•–—›'
 #    POST = %w| ] } ' ` " ) , ; : \ ! \ ? \ % ‚ „ … † ‡ ‰ ‹ ‘ ’ “ ” • – — › |
-    POST = %w{! ? , : ; . )}
-    PRE = %w{(}
+    
+    SIMPLE_PRE = [] # spanish marks
+    PAIR_PRE = ['(', '{', '[']
+    SIMPLE_POST = ['!', '?', ',', ':', ';', '.']
+    PAIR_POST = [')', '}', ']']
+    PRE_N_POST = ['"', "'"]
 
+    PRE = SIMPLE_PRE + PAIR_PRE
+    POST = SIMPLE_POST + PAIR_POST
+    
     def initialize(lang=:de)
       @lang = lang
+      @braces = []
     end
 
     def tokenize(str)
       tokens = []
       token = ''
       output = ''
-      fields = str.split(FS)
+      fields = str.chomp.split(FS)
+#      puts "Fields: #{fields}"
+      if fields.empty?
+        return [""]
+      end
+      
       fields.each do |field|
-        field.each_char do |ch|
-         if POST.include?(ch)
-            output << "\n#{ch}"
-          elsif PRE.include?(ch)
+        field.each_char.with_index do |ch, idx|
+          case
+          when PRE.include?(ch)
             output << "#{ch}\n"
+          when POST.include?(ch)
+            output << "\n#{ch}"
+            if ['?', '!', '.'].include?(ch)
+              output << "\n"
+            end
+          when PRE_N_POST.include?(ch)
+            if idx == 0
+              output << "#{ch}\n"
+            elsif idx != 0
+              output << "\n#{ch}"
+            end
           else
             output << ch
           end
         end
-        output << "\n"
+       output << "\n"
       end
-      output.split("\n")
+#      puts "String: #{output.inspect}"
+      out = output.split("\n", -1)
+#      puts "Output: #{out}"
+
+      out
     end
 
   end # class
