ecfb973423168b4ecdd9f3c83b375339c9dd530a
joshweir <joshua.weir@gmail.com>
2017-03-31 17:48:04 +1100

    fix #6

--- test/regression_tests/test_de_tokenizer.rb
+++ test/regression_tests/test_de_tokenizer.rb
@@ -31,6 +31,69 @@ class TestTokenizer < Minitest::Test
     output = @t.tokenize(input)
     assert_equal(etalon, output)
   end
+
+  def test_tokenization_003
+    input = 'Try some code: test(this).'
+    etalon = %w(Try some code : test ( this ) .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_004
+    input = 'Try an email: test.email@example.com.'
+    etalon = %w(Try an email : test.email@example.com .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_005
+    input = "et souligne 'l'interrelation étroite de l'imagerie' avec le comportement."
+    etalon = %w(et souligne ' l'interrelation étroite de l'imagerie ' avec le comportement .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_006
+    input = 'Try some code: test(inner(brackets)also).'
+    etalon = %w(Try some code : test ( inner ( brackets ) also ) .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_007
+    input = 'Try some code: test[(inner(brackets)also)].'
+    etalon = %w(Try some code : test [ ( inner ( brackets ) also ) ] .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_008
+    input = "Check single quotes: 'quoted string'."
+    etalon = %w(Check single quotes : ' quoted string ' .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_009
+    input = "Check silly embedded single quotes: 'quoted 'embedded string' string'."
+    etalon = %w(Check silly embedded single quotes : ' quoted ' embedded string ' string ' .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_010
+    input = "Check quotes: ('test quotes')."
+    etalon = %w(Check quotes : ( ' test quotes ' ) .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
+
+  def test_tokenization_011
+    input = "Check quotes: (''test quotes'')."
+    etalon = %w(Check quotes : ( ' ' test quotes ' ' ) .)
+    output = @t.tokenize(input)
+    assert_equal(etalon, output)
+  end
 end
 
 describe Tokenizer do
